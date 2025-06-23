50e67fb629f3ebcfb79484d48993365514d150a8
alexander-brady <bradya@student.ethz.ch>
2025-06-23 23:20:41 +0200

    More results

--- scripts/eval_array.sh
+++ scripts/eval_array.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=s1_eval_array
-#SBATCH --output=logs/def_eval_%j_%a.out
-#SBATCH --error=logs/def_eval_%j_%a.err
+#SBATCH --output=logs/third_eval_%j_%a.out
+#SBATCH --error=logs/third_eval_%j_%a.err
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=16G
 #SBATCH --nodes=1
@@ -9,16 +9,18 @@
 #SBATCH --gres=gpumem:64g
 #SBATCH --time=24:00:00
 #SBATCH --mail-type=END,FAIL
-#SBATCH --array=1-5,7-10
+#SBATCH --array=1-3,5-10
 
 MODEL="s1.1-1.5B" # From simplescaling repo
 MAX_BUDGET_FORCING_STEPS=${SLURM_ARRAY_TASK_ID} # Use the array task ID as the max budget forcing steps
 WAIT_TOKEN="Wait"
 
+ABLATION="third"
+
 # SCALE_FUNCTION="default"
 # SCALE_FUNCTION="entropy_thresholding"
 SCALE_FUNCTION="uncertainty_driven_reevaluation"
-OUTPUT_PATH="../../results/$USER/${SCALE_FUNCTION}_${MAX_BUDGET_FORCING_STEPS}_${SLURM_JOB_ID}"
+OUTPUT_PATH="../../results/$USER/${SCALE_FUNCTION}_ablation_${ABLATION}_${MAX_BUDGET_FORCING_STEPS}_${SLURM_JOB_ID}"
 
 module load stack/2024-06 gcc/12.2.0 python/3.11.6 cuda/11.3.1 eth_proxy
 
@@ -68,7 +70,7 @@ OPENAI_API_KEY=$OPENAI_API_KEY PROCESSOR="gpt-4o-mini" lm_eval \
     --apply_chat_template \
     --output_path "$OUTPUT_PATH" \
     --log_samples \
-    --gen_kwargs "max_gen_toks=32768,max_tokens_thinking=auto,thinking_n_ignore=$MAX_BUDGET_FORCING_STEPS,thinking_n_ignore_str=$WAIT_TOKEN,scale_func_name=$SCALE_FUNCTION" \
+    --gen_kwargs "max_gen_toks=32768,max_tokens_thinking=auto,thinking_n_ignore=$MAX_BUDGET_FORCING_STEPS,thinking_n_ignore_str=$WAIT_TOKEN,scale_func_name=$SCALE_FUNCTION,ablation=$ABLATION" \
     # --limit 10
 
 echo "Job completed at $(date)"
